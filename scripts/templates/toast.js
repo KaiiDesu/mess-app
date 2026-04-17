a6c9f592ea26581aa63cd4f60a2e434ea18f57dc
KaiiDesu <112854298+KaiiDesu@users.noreply.github.com>
2026-04-17 10:00:10 +0800

    Updated

--- scripts/templates/toast.js
+++ scripts/templates/toast.js
@@ -2,10 +2,10 @@
 window.ZapTemplateParts.toast = String.raw`
 <!-- TOAST NOTIFICATION -->
     <div class="toast" id="toast">
-      <div class="toast-av">😄</div>
+      <div class="toast-av"></div>
       <div class="toast-info">
-        <div class="toast-name">Alex Rivera</div>
-        <div class="toast-msg">🎵 sent a voice message</div>
+        <div class="toast-name"></div>
+        <div class="toast-msg"></div>
       </div>
     </div>
 `;
